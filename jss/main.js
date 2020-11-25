6b78358522c677f67689fff7f2fa0b1fe34ce581
Vašek Lacheta <47534064+Vistoun@users.noreply.github.com>
2020-11-25 10:39:30 +0100

    responsive

--- jss/main.js
+++ jss/main.js
@@ -1,7 +1,8 @@
+/*
 const articles = [
     {
        "title": "Kdo jsme",
-       "text": "orem ipsum dolor sit, amet consectetur adipisicing elit. Odio harum fugit assumenda dolore deleniti eos laborum ducimus suscipit delectus vel quo repellat optio provident atque impedit, quisquam, quos magni qui.Tempora doloribus nam similique, suscipit voluptate earum aspernatur quasi, ducimus, blanditiis eligendi inventore sit expedita nisi dolorem illo asperiores voluptatem commodi nesciunt molestias eius impedit? Quas dolorum cupiditate ad odio.Animi dolores incidunt iure consequuntur iusto officiis eum. Rerum, aperiam cupiditate fuga saepe sunt qui soluta temporibus nul"
+       "text": ""
  
     },
     {
@@ -13,7 +14,7 @@ const articles = [
        "text": "Hřbitov La Recoleta je hřbitov v Buenos Aires. Obsahuje hroby slavných lidí..."
     }
  ];
- 
+ /*
  $("#navBox").linkUnderlineAnim({
       "speed": 250,    
       "color":"#FFFFFF",        
@@ -22,7 +23,11 @@ const articles = [
     });
     
 
+    
+
 $(document).ready(function () {
+
+   
  articles.forEach((articleTitle) => {
     $("#navBox").append(`<li><a>${articleTitle.title}</a></li>`);
  });
